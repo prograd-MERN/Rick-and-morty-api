e66c25b54646f3adbaba26554e06fbb9886d8e81
Balaramansethu <baalaaraman@gmail.com>
2023-09-14 13:35:00 +0530

    Initial commit for saving the file structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RickAndMortyNameComponents from './components/RickAndMortyNameComponents/RickAndMortyNameComponents'
+
+const App = () => {
+  return (
+<React.Fragment>
+  <RickAndMortyNameComponents/>
+</React.Fragment>  )
+}
+
+export default App
---
+++ src/components/RickAndMortyNameComponents/RickAndMortyNameComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RickAndMortyNameComponents = () => {
+  return (
+    <div>RickAndMortyNameComponents</div>
+  )
+}
+
+export default RickAndMortyNameComponents
